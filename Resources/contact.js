32b0d2f5b994721d62bd5fd9a64673d558414e9c
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-05-26 12:25:00 +0100

    update

--- Resources/contact.js
+++ Resources/contact.js
@@ -1,9 +1,4 @@
-function toggleDarkMode(event) {
-    const isDarkmode = event.target.checked
-  
-    if (isDarkmode) {
-      document.documentElement.setAttribute('data-mode', 'dark')
-    } else {
-      document.documentElement.setAttribute('data-mode', '')
-    }
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
   }
