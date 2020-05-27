bac400b8231f429032b8b190834218625e4d506c
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-05-27 07:54:09 +0100

    update

--- Resources/script.js
+++ Resources/script.js
@@ -1,9 +1,11 @@
+var i = 0;
+var txt = 'Hello, I am Tolu.';
+var speed = 50;
 
-const typed = new Typed('#typed', {
-  stringsElement: '#typed-strings',
-  startDelay: 700,
-  loop: true,
-  typeSpeed: 39,
-  backSpeed: 20,
-  backDelay: 3400,
-});
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("name-carousel").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
