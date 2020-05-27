94800a8bc70206434912ceccf5d347a7f91a4fac
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-05-27 20:57:16 +0100

    update

--- Resources/script.js
+++ Resources/script.js
@@ -1,9 +1,2 @@
-import Typed from 'typed.js';
 
-var options = {
-  strings: ['<i>Hello </i> there!', '&amp; I am Tolu.'],
-  typeSpeed: 40
-};
-
-var typed = new Typed('.name-carousel', options);
 
