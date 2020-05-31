8ec6d13a9a99c51379ac7283e79161e807f16742
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-05-31 13:09:55 +0100

    Update script.js

--- Resources/script.js
+++ Resources/script.js
@@ -1,12 +1,3 @@
-var typed = new Typed('#typed', {
-  stringsElement: '#name-carousel',
-  startDelay: 700,
-  loop: true,
-  typeSpeed: 39,
-  backSpeed: 20,
-  backDelay: 3400,
-});
-
 function myFunction() {
   var x = document.getElementById("my-navbar");
   if (x.className === "nav-bar") {
@@ -15,3 +6,12 @@ function myFunction() {
     x.className = "nav-bar";
   }
 }
+
+var typed = new Typed('#typed', {
+  stringsElement: '#name-carousel',
+  startDelay: 700,
+  loop: true,
+  typeSpeed: 39,
+  backSpeed: 20,
+  backDelay: 3400,
+});
