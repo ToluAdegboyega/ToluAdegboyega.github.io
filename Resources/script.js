b2c2e3187d52bb04005aac9b1e8e5178b1de0b41
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-05-31 13:27:09 +0100

    js update

--- Resources/script.js
+++ Resources/script.js
@@ -1,11 +1,3 @@
-function myFunction() {
-  var x = document.getElementById("my-navbar");
-  if (x.className === "nav-bar") {
-    x.className += " responsive";
-  } else {
-    x.className = "nav-bar";
-  }
-}
 
 var typed = new Typed('#typed', {
   stringsElement: '#name-carousel',
