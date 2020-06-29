4639207ccdc54c0d6b5812d9a00b808775634a79
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-06-29 17:30:14 +0100

    slide test update

--- Resources/script.js
+++ Resources/script.js
@@ -7,3 +7,9 @@ var typed = new Typed('#typed', {
   backSpeed: 20,
   backDelay: 3400,
 });
+
+function script(){
+  setTimeout(function(){
+      document.getElementById('test').style.left = '1000px'; // new left position is 1000px in this example
+  }, 2000); // 2000 = 2 seconds after page load
+}
