ff9d7b0ce4cef08b8f05c18e05d1e19861a368a7
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-06-29 17:44:53 +0100

    update

--- Resources/script.js
+++ Resources/script.js
@@ -8,8 +8,3 @@ var typed = new Typed('#typed', {
   backDelay: 3400,
 });
 
-function script(){
-  setTimeout(function(){
-      document.getElementById('test').style.left = '1000px'; // new left position is 1000px in this example
-  }, 2000); // 2000 = 2 seconds after page load
-}
