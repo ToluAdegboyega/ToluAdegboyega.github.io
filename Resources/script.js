489fb46a219b1fb0239647468533fa04e9f4c9ce
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-09-11 18:52:58 +0100

    pre page loader and animated slide up update

--- Resources/script.js
+++ Resources/script.js
@@ -8,3 +8,17 @@ var typed = new Typed('#typed', {
   backDelay: 3400,
 });
 
+const loading = document.querySelector('.loading');
+const main = document.querySelector('.main-main');
+
+function init() {
+    setTimeout( ()=> {
+        loading.style.opacity = 0;
+        loading.style.display = 'none';
+
+        main.style.display = 'block';
+        main.style.opacity = 1;
+    }, 4000);
+}
+
+init();
